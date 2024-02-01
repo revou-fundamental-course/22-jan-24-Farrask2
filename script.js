9883c1ff97c48a59f9cddedc88589c3c9a0d5975
Farrask2 <157378932+Farrask2@users.noreply.github.com>
2024-02-01 20:34:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Your message has been sent. We will get back to you as soon as possible.");
+  });
+  
+  document.getElementById("packageForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for subscribing to our newsletter. You will receive exclusive deals and discounts soon.");
+  });
